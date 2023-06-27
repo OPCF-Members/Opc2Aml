02e7ac3b0ffd7e5ec30fa35c97220f0fb45a67f0
Archie Miller <62433534+Archie-Miller@users.noreply.github.com>
2023-06-27 13:00:10 -0600

    Complete recursive test

--- SystemTest/Ids.cs
+++ SystemTest/Ids.cs
@@ -146,6 +146,7 @@ namespace SystemTest
                 {
                     elements.Add(potentialGuid, path);
                 }
+                WalkInstanceHierarchy(internalElement, interfaces, elements, next);
             }
         }
 
