759c78039e311a17c24fab1a0cf50f8216b671e6
Archie <archie@dreamsandadventures.ca>
2025-09-24 12:16:18 -0600

    Remove Another Validation as it has been moved to Validation File

--- SystemTest/TestOptionSetEmptyValues.cs
+++ SystemTest/TestOptionSetEmptyValues.cs
@@ -14,47 +14,6 @@ namespace SystemTest
     {
         #region Tests
 
-        [ TestMethod]
-        public void Validation()
-        {
-            var lookupService = LookupService.Register();
-            var service = ValidatorService.Register();
-
-            IEnumerable<ValidationElement> issues = service.ValidateAll(GetDocument());
-            if (issues != null)
-            {
-                Assert.AreEqual(0, issues.AliasReferenceValidationResults().Count(), "AliasReferenceValidationResults");
-                Assert.AreEqual(0, issues.ExternalReferenceValidationResults().Count(), "ExternalReferenceValidationResults");
-                Assert.AreEqual(0, issues.ClassPathValidationResults().Count(), "ClassPathValidationResults");
-                Assert.AreEqual(0, issues.IDReferenceValidationResults().Count(), "IDReferenceValidationResults");
-                Assert.AreEqual(0, issues.PathReferenceValidationResults().Count(), "PathReferenceValidationResults");
-                Assert.AreEqual(0, issues.NameReferenceValidationResults().Count(), "NameReferenceValidationResults");
-                Assert.AreEqual(0, issues.NameValidationResults().Count(), "NameValidationResults");
-                Assert.AreEqual(0, issues.IDValidationResults().Count(), "IDValidationResults");
-                Assert.AreEqual(0, issues.MetaDataValidationResults().Count(), "MetaDataValidationResults");
-                Assert.AreEqual(0, issues.InvalidAutomationMLVersion().Count(), "InvalidAutomationMLVersion");
-                Assert.AreEqual(0, issues.InvalidSchemaVersion().Count(), "InvalidSchemaVersion");
-                Assert.AreEqual(0, issues.MissingDocumentSourceInformation().Count(), "MissingDocumentSourceInformation");
-                Assert.AreEqual(0, issues.InvalidIDs().Count(), "InvalidIDs");
-                Assert.AreEqual(0, issues.NotUniquePath().Count(), "NotUniquePath");
-                Assert.AreEqual(0, issues.Repairable().Count(), "Repairable");
-                Assert.AreEqual(0, issues.NotRepairable().Count(), "NotRepairable");
-                Assert.AreEqual(0, issues.UnresolvedIDRefs().Count(), "UnresolvedIDRefs");
-                Assert.AreEqual(0, issues.UnresolvedPathRefs().Count(), "UnresolvedPathRefs");
-                Assert.AreEqual(0, issues.EmptyPathRefs().Count(), "EmptyPathRefs");
-                Assert.AreEqual(0, issues.EmptyIDRefs().Count(), "EmptyIDRefs");
-                Assert.AreEqual(0, issues.MissingIDs().Count(), "MissingIDs");
-                Assert.AreEqual(0, issues.NotUniqueIDs().Count(), "NotUniqueIDs");
-                Assert.AreEqual(0, issues.UnidentifiedAlias().Count(), "UnidentifiedAlias");
-                Assert.AreEqual(0, issues.UselessAlias().Count(), "UselessAlias");
-                Assert.AreEqual(0, issues.Count(), "TotalIssues");
-            }
-            else
-            {
-                Assert.Fail("Unable to get validation results");
-            }
-        }
-
         [TestMethod, Timeout( TestHelper.UnitTestTimeout )]
         public void TestOperationalHealthOptionSet()
         {
